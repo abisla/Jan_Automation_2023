16d5354828bccb5e73e45f65ab2a44f7776ed3d4
abisla <86798738+abisla@users.noreply.github.com>
2023-04-18 13:25:10 -0400

    test 4/18 pt 2

--- src/test/java/Day1_022523/T1_JavaClass.java
+++ src/test/java/Day1_022523/T1_JavaClass.java
@@ -3,6 +3,7 @@ package Day1_022523;
 public class T1_JavaClass {
     //this is my first java class
     //comment for github
-    //test for github
+
+    //new person committed the code to github
 
 } //end of java
